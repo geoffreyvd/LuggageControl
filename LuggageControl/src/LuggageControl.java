b320e9beebf2064be276fc54b0742bc62437a84b
Dantali0n <corne.lukken@hotmail.com>
2015-11-10 11:56:11 +0100

    Initial commit

---
+++ LuggageControl/src/LuggageControl.java
@@ -0,0 +1,22 @@
+/**
+ * Root class off application
+ * @author Team3 Fys
+ */
+public class LuggageControl {
+    
+    /**
+     * 
+     */
+    public LuggageControl() {
+        
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // circumventing static reference
+        new LuggageControl();
+    }
+    
+}
--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# BaggageControl
+# LuggageControl
+
+Algemene readme file, hier geen documentatie plaatsen.
+Documentatie wordt geplaatst in javadoc.
+Cookbook wordt geplaatst op de wiki.
