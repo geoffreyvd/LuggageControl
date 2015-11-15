75340b5b79a034c6e4231fc3055c3951563c13c2
geoffreyvd <geoffrey.v.d@live.nl>
2015-11-15 17:41:18 +0100

    Improved Database manager, Added DataBaseQueryExample
    #13 - New method in class DatabaseMan, 2 examples of said method and
    existing method in new file; DataBaseQueryExample

--- LuggageControl/src/managers/DatabaseMan.java
+++ LuggageControl/src/managers/DatabaseMan.java
@@ -14,6 +14,9 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 
 /**
+ * DataBaseManager is an class with predefined credentials for the database. It
+ * also provides a Query method which can be used to query directly to the
+ * database.
  *
  * @author geoffrey
  */
@@ -32,18 +35,39 @@ public class DatabaseMan {
      * access database with query
      *
      * @param query
+     * @return ResultSet result
      */
-    public void Query(String query) {
+    public ResultSet Query(String query) {
         try {
             Connection connection = DriverManager.getConnection(
                     "jdbc:mysql://" + HOST_NAME + "/" + DATABASE_NAME, DATABASE_USERNAME, DATABASE_PASSWORD);
             Statement statement = connection.createStatement();
             ResultSet result = statement.executeQuery(query);
-            while (result.next()) {
-                System.out.println(result.getString(1) + " " + result.getString(2) + " " + result.getString(3));
-            }
+            return result;
         } catch (SQLException ex) {
             Logger.getLogger(DatabaseMan.class.getName()).log(Level.SEVERE, null, ex);
+            return null;
+        }
+    }
+
+    /**
+     * This query will return one value, a string: It returns the value from the
+     * first column and the first row.
+     *
+     * @param query
+     * @return String value
+     */
+    public String QueryOneResult(String query) {
+        try {
+            Connection connection = DriverManager.getConnection(
+                    "jdbc:mysql://" + HOST_NAME + "/" + DATABASE_NAME, DATABASE_USERNAME, DATABASE_PASSWORD);
+            Statement statement = connection.createStatement();
+            ResultSet result = statement.executeQuery(query);
+            result.next();
+            return result.getString(1);
+        } catch (SQLException ex) {
+            Logger.getLogger(DatabaseMan.class.getName()).log(Level.SEVERE, null, ex);
+            return null;
         }
     }
 }
