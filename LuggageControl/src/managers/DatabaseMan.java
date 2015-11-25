1b59e6a68ab3bd15c792e1e0f58c6880cc905b2f
Dantali0n <corne.lukken@hotmail.com>
2015-11-25 16:09:05 +0100

    Partial working linux db export

--- LuggageControl/src/managers/DatabaseMan.java
+++ LuggageControl/src/managers/DatabaseMan.java
@@ -8,11 +8,13 @@ package managers;
 import baseClasses.ErrorJDialog;
 import java.awt.HeadlessException;
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
@@ -53,33 +55,51 @@ public class DatabaseMan {
     }
 
     public static void exportDatabase(LuggageControl luggageControl) {
+        JTextField username = new JTextField(5);
+        JPasswordField password = new JPasswordField(5);
+        JPanel myPanel = new JPanel();
+        myPanel.add(new JLabel("Username:"));
+        myPanel.add(username);
+        myPanel.add(Box.createHorizontalStrut(15)); // a spacer
+        myPanel.add(new JLabel("Password:"));
+        myPanel.add(password);
+        
+        int result = JOptionPane.showConfirmDialog(null, myPanel,
+            "Please Enter your mysql username and password", JOptionPane.OK_CANCEL_OPTION
+        );
+        
+        String line = null;
+        File file = null;
+            
         if (System.getProperty("os.name").equals("Linux")) {
             Runtime rt = Runtime.getRuntime();
-            String[] commands = {"/bin/sh", "-c", "mysqldump -u lugcontroluser -p -r gucci.sql luggagecontroldata", "verysecure"};
+            // yes yes we will just use echo to pipe your password through to mysqldump this is so secure lol.
+            // yet is the only way since password prompts ignor stdin and similiar and require a physical hid device as input
+            // now we could hack our way into a virtual usb hid device but echo is much more simple and elegant.
+            String[] commands = {"/bin/sh", "-c", "echo '" + password.getText() + "' | mysqldump -u " + username.getText() + " -p -r gucci.sql luggagecontroldata"};
             Process proc;
             try {
                 proc = rt.exec(commands);
                 BufferedReader stdInput = new BufferedReader(new InputStreamReader(proc.getInputStream()));
+                
+                if((line = stdInput.readLine()) != null) {
+                    System.out.println(line);
+                }
+                stdInput.close();
 
                 BufferedReader stdError = new BufferedReader(new InputStreamReader(proc.getErrorStream()));
+                
+                if((line = stdError.readLine()) != null) {
+                    System.out.println(line);
+                }
+                stdError.close();
+
 
             } catch (Exception e) {
                 e.printStackTrace();
             }
-        } else {
-            JTextField username = new JTextField(5);
-            JPasswordField password = new JPasswordField(5);
-            JPanel myPanel = new JPanel();
-            myPanel.add(new JLabel("Username:"));
-            myPanel.add(username);
-            myPanel.add(Box.createHorizontalStrut(15)); // a spacer
-            myPanel.add(new JLabel("Password:"));
-            myPanel.add(password);
-            File file = null;
-            String file2 = "";
-
-            int result = JOptionPane.showConfirmDialog(null, myPanel,
-                    "Please Enter your mysql username and password", JOptionPane.OK_CANCEL_OPTION);
+        } 
+        else {
             if (result == JOptionPane.OK_OPTION) {
                 JFileChooser fileSaver = new JFileChooser();
                 fileSaver.setFileFilter(new FileNameExtensionFilter("Mysqldump File (.sql)", "sql"));
@@ -94,11 +114,10 @@ public class DatabaseMan {
                             file = new File(fileSaver.getSelectedFile() + ".sql");
                         }
 
-
                         FileWriter fileWriter = new FileWriter(file);
                         fileWriter.close();
-                        
-                    } catch (Exception e) {
+                    } 
+                    catch (Exception e) {
                         new ErrorJDialog(luggageControl, true, e.getMessage(), e.getStackTrace());
                     }
                 }
@@ -131,7 +150,6 @@ public class DatabaseMan {
 //                    }
 //                }
                 try {
-                    String line = null;
                     Process process2 = Runtime.getRuntime().exec("CMD /C " + ConfigurationMan.getMysqlDumpLocationWindows(luggageControl)+ " luggagecontroldata -u" + username.getText() + " -p" + password.getText() + " -r" + file);
                     BufferedReader bri = new BufferedReader(new InputStreamReader(process2.getInputStream()));
                     BufferedReader bre = new BufferedReader(new InputStreamReader(process2.getErrorStream()));
